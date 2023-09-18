d677ad6e4212620601d3952ff8106e971b23087a
toan.dg <toandinhgia@tcom.vn>
2023-09-18 15:46:10 +0700

    add all files

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index() {
+
+        $products = Product::where('is_feature', 'Yes')->orderBy('id', 'DESC')->where('status', 1)->take(8)->get();
+        $data['featuredProducts'] = $products;
+
+        $latestProducts = Product::orderBy('id', 'DESC')->where('status', 1)->take(8)->get();
+        $data['latestProducts'] = $latestProducts;
+
+        return view('front.home', $data);
+    }
+}
---
+++ app/Http/Controllers/admin/ProductImageController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductImage;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class ProductImageController extends Controller
+{
+    public function update(Request $request) {
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $sourcePath = $image->getPathName();
+
+        $productImage = new ProductImage();
+        $productImage->product_id = $request->product_id;
+        $productImage->image = 'NULL';
+        $productImage->save();
+
+        $imageName = $request->product_id.'-'.$productImage->id.'-'.time().'.'.$ext;
+
+        // product_id => 4, product_image_id => 1
+        // 4-1-26742374729349.jpg
+
+        $productImage->image = $imageName;
+        $productImage->save();
+
+        // Large Image
+        $destPath = public_path().'/uploads/product/large/'.$imageName;
+        $image = Image::make($sourcePath);
+        $image->resize(1400, null, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        $image->save($destPath);
+        
+        // Small Image
+        $destPath = public_path().'/uploads/product/small/'.$imageName;
+        $image = Image::make($sourcePath);
+        $image->fit(300,300);
+        $image->save($destPath);
+
+        return response()->json([
+            'status' => true,
+            'image_id' => $productImage->id,
+            'ImagePath' => asset('uploads/product/small/'.$productImage->image),
+            'message' => 'Image saved successfully.',
+        ]);
+    }
+
+    public function destroy(Request $request) {
+        $productImage = ProductImage::find($request->id);
+
+        if (empty($productImage)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Image not found.',
+            ]);
+        }
+
+        // Delete images from controller
+        File::delete(public_path('uploads/product/large/'.$productImage->image));
+        File::delete(public_path('uploads/product/small/'.$productImage->image));
+        $productImage->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Image deleted successfully.',
+        ]);
+    }
+}

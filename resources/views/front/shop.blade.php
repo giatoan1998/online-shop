c85fa5213159dbc0640d128f5a4c1694d49f135d
toan.dg <toan.dg>
2023-09-22 11:33:34 +0700

    fix bug

--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -79,30 +79,7 @@
 
                     <div class="card">
                         <div class="card-body">
-                            <div class="form-check mb-2">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    $0-$100
-                                </label>
-                            </div>
-                            <div class="form-check mb-2">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    $100-$200
-                                </label>
-                            </div>
-                            <div class="form-check mb-2">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    $200-$500
-                                </label>
-                            </div>
-                            <div class="form-check mb-2">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    $500+
-                                </label>
-                            </div>
+                            <input type="text" class="js-range-slider" name="my_range" value="" />
                         </div>
                     </div>
                 </div>
@@ -111,36 +88,11 @@
                         <div class="col-12 pb-1">
                             <div class="d-flex align-items-center justify-content-end mb-4">
                                 <div class="ml-2">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-bs-toggle="dropdown">Sorting</button>
-                                        <div class="dropdown-menu dropdown-menu-right">
-                                            <a class="dropdown-item" href="#">Latest</a>
-                                            <a class="dropdown-item" href="#">Price High</a>
-                                            <a class="dropdown-item" href="#">Price Low</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="card product-card">
-                                <div class="product-image position-relative">
-                                    <a href="" class="product-img"><img class="card-img-top" src="images/product-1.jpg" alt=""></a>
-                                    <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
-
-                                    <div class="product-action">
-                                        <a class="btn btn-dark" href="#">
-                                            <i class="fa fa-shopping-cart"></i> Add To Cart
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="card-body text-center mt-3">
-                                    <a class="h6 link" href="product.php">Dummy Product Title</a>
-                                    <div class="price mt-2">
-                                        <span class="h5"><strong>$100</strong></span>
-                                        <span class="h6 text-underline"><del>$120</del></span>
-                                    </div>
+                                    <select name="sort" id="sort" class="form-control">
+                                        <option value="latest" {{ ($sort == 'latest') ? 'selected' : '' }}>Latest</option>
+                                        <option value="price_desc" {{ ($sort == 'price_desc') ? 'selected' : '' }}>Price High</option>
+                                        <option value="price_asc" {{ ($sort == 'price_asc') ? 'selected' : '' }}>Price Low</option>
+                                    </select>
                                 </div>
                             </div>
                         </div>
@@ -160,7 +112,7 @@
                                                     <img class="card-img-top" src="{{ asset('admin-assets/img/default-150x150.png') }}">
                                                 @endif
                                             </a>
-                                            
+
                                             <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
 
                                             <div class="product-action">
@@ -207,7 +159,29 @@
 
 @section('customJs')
 <script>
+
+    rangeSlider = $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: {{ ($priceMin) }},
+        step: 10,
+        to: {{ ($priceMax) }},
+        skin: "round",
+        max_postfix: "+",
+        prefix: "$",
+        onFinish: function() {
+            apply_filter();
+        }
+    });
+
+    var slider = $(".js-range-slider").data("ionRangeSlider");
+
     $(".brand-label").change(function() {
+        apply_filter();
+    });
+
+    $("#sort").change(function() {
         apply_filter();
     });
 
@@ -221,10 +195,20 @@
             }
         })
 
-        console.log(brands.toString());
-
         var url = '{{ url()->current() }}?';
-        window.location.href = url + '&brand=' + brands.toString();
+
+        // Brand filter
+        if (brands.length > 0) {
+            url += '&brand=' + brands.toString();
+        }
+
+        // Price Range Filter
+        url += '&price_min='+slider.result.from+'&price_max=' + slider.result.to;
+
+        // Sorting filter
+        url += '&sort='+$("#sort").val();
+
+        window.location.href = url;
     }
 </script>
 @endsection

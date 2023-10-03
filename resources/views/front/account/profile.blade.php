42639d5b441d3ba5e89f7d46a357fba2514ffe9b
toan.dg <toan.dg>
2023-10-03 10:39:05 +0700

    add code 45

--- resources/views/front/account/profile.blade.php
+++ resources/views/front/account/profile.blade.php
@@ -15,6 +15,9 @@
 <section class=" section-11 ">
     <div class="container  mt-5">
         <div class="row">
+            <div class="col-md-12">
+                @include('front.account.common.message')
+            </div>
             <div class="col-md-3">
                 @include('front.account.common.slidebar')
             </div>
@@ -23,34 +26,234 @@
                     <div class="card-header">
                         <h2 class="h5 mb-0 pt-2 pb-2">Personal Information</h2>
                     </div>
-                    <div class="card-body p-4">
-                        <div class="row">
-                            <div class="mb-3">
-                                <label for="name">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Enter Your Name" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="email">Email</label>
-                                <input type="text" name="email" id="email" placeholder="Enter Your Email" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="phone">Phone</label>
-                                <input type="text" name="phone" id="phone" placeholder="Enter Your Phone" class="form-control">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="phone">Address</label>
-                                <textarea name="address" id="address" class="form-control" cols="30" rows="5" placeholder="Enter Your Address"></textarea>
-                            </div>
-
-                            <div class="d-flex">
-                                <button class="btn btn-dark">Update</button>
+                    <form action="" name="profileForm" id="profileForm">
+                        <div class="card-body p-4">
+                            <div class="row">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input value="{{ $user->name }}" type="text" name="name" id="name" placeholder="Enter Your Name" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="email">Email</label>
+                                    <input value="{{ $user->email }}" type="text" name="email" id="email" placeholder="Enter Your Email" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="phone">Phone</label>
+                                    <input value="{{ $user->phone }}" type="text" name="phone" id="phone" placeholder="Enter Your Phone" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="d-flex">
+                                    <button class="btn btn-dark">Update</button>
+                                </div>
                             </div>
                         </div>
+                    </form>
+                </div>
+
+                <div class="card mt-5">
+                    <div class="card-header">
+                        <h2 class="h5 mb-0 pt-2 pb-2">Address</h2>
                     </div>
+                    <form action="" name="addressForm" id="addressForm">
+                        <div class="card-body p-4">
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="first_name">First Name</label>
+                                    <input value="{{ (!empty($address)) ? $address->first_name : '' }}" type="text" name="first_name" id="first_name" placeholder="Enter Your First Name" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="last_name">Last Name</label>
+                                    <input value="{{ (!empty($address)) ? $address->last_name : '' }}" type="text" name="last_name" id="last_name" placeholder="Enter Your Last Name" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="email">Email</label>
+                                    <input value="{{ (!empty($address)) ? $address->email : '' }}" type="text" name="email" id="email" placeholder="Enter Your Email" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="mobile">Mobile</label>
+                                    <input value="{{ (!empty($address)) ? $address->mobile : '' }}" type="text" name="mobile" id="mobile" placeholder="Enter Your Mobile No." class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="country_id">Country</label>
+                                    <select name="country_id" id="country_id" class="form-control">
+                                        <option value="">Select a Country</option>
+                                        @if ($countries->isNotEmpty())
+                                            @foreach ($countries as $country)
+                                                <option {{ (!empty($address) && $address->country_id == $country->id) ? 'selected' : '' }} value="{{ $country->id }}">{{ $country->name }}</option>
+                                            @endforeach
+                                        @endif
+                                    </select>
+                                    <p></p>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="address">Address</label>
+                                    <textarea name="address" id="address" cols="20" rows="5" class="form-control">{{ (!empty($address)) ? $address->address : '' }}</textarea>
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="apartment">Apartment</label>
+                                    <input value="{{ (!empty($address)) ? $address->apartment : '' }}" type="text" name="apartment" id="apartment" placeholder="Apartment" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="city">City</label>
+                                    <input value="{{ (!empty($address)) ? $address->city : '' }}" type="text" name="city" id="city" placeholder="City" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="state">State</label>
+                                    <input value="{{ (!empty($address)) ? $address->state : '' }}" type="text" name="state" id="state" placeholder="State" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="zip">Zip</label>
+                                    <input value="{{ (!empty($address)) ? $address->zip : '' }}" type="text" name="zip" id="zip" placeholder="Zip" class="form-control">
+                                    <p></p>
+                                </div>
+                                <div class="d-flex">
+                                    <button class="btn btn-dark">Update</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </section>
+@endsection
+@section('customJs')
+    <script>
+        $("#profileForm").submit(function(event) {
+            event.preventDefault();
+
+            $.ajax({
+                url: '{{ route("account.updateProfile") }}',
+                type: 'post',
+                data: $(this).serializeArray(),
+                dataType: 'json',
+                success: function(response) {
+
+                    if (response.status == true) {
+                        window.location.href = '{{ route("account.profile") }}';
+
+                        $("#profileForm #name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#profileForm #email").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#profileForm #phone").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                    } else {
+                        var errors = response.errors;
+
+                        if (errors.name) {
+                            $("#profileForm #name").addClass('is-invalid').siblings('p').html(errors.name).addClass('invalid-feedback');
+                        } else {
+                            $("#profileForm #name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.email) {
+                            $("#profileForm #email").addClass('is-invalid').siblings('p').html(errors.email).addClass('invalid-feedback');
+                        } else {
+                            $("#profileForm #email").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.phone) {
+                            $("#profileForm #phone").addClass('is-invalid').siblings('p').html(errors.phone).addClass('invalid-feedback');
+                        } else {
+                            $("#profileForm #phone").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+                    }
+                }
+            })
+        });
+
+        // addressForm
+        $("#addressForm").submit(function(event) {
+            event.preventDefault();
+
+            $.ajax({
+                url: '{{ route("account.updateAddress") }}',
+                type: 'post',
+                data: $(this).serializeArray(),
+                dataType: 'json',
+                success: function(response) {
+
+                    if (response.status == true) {
+                        window.location.href = '{{ route("account.profile") }}';
+
+                        $("#addressForm #first_name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #last_name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #email").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #mobile").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #country_id").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #address").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #city").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #state").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        $("#addressForm #zip").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                    } else {
+                        var errors = response.errors;
+
+                        if (errors.first_name) {
+                            $("#addressForm #first_name").addClass('is-invalid').siblings('p').html(errors.first_name).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #first_name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.last_name) {
+                            $("#addressForm #last_name").addClass('is-invalid').siblings('p').html(errors.last_name).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #last_name").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.email) {
+                            $("#addressForm #email").addClass('is-invalid').siblings('p').html(errors.email).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #email").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.mobile) {
+                            $("#addressForm #mobile").addClass('is-invalid').siblings('p').html(errors.mobile).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #mobile").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.country_id) {
+                            $("#addressForm #country_id").addClass('is-invalid').siblings('p').html(errors.country_id).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #country_id").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.address) {
+                            $("#addressForm #address").addClass('is-invalid').siblings('p').html(errors.address).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #address").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.state) {
+                            $("#addressForm #state").addClass('is-invalid').siblings('p').html(errors.state).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #state").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.city) {
+                            $("#addressForm #city").addClass('is-invalid').siblings('p').html(errors.city).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #city").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+
+                        if (errors.zip) {
+                            $("#addressForm #zip").addClass('is-invalid').siblings('p').html(errors.zip).addClass('invalid-feedback');
+                        } else {
+                            $("#addressForm #zip").removeClass('is-invalid').siblings('p').html('').removeClass('invalid-feedback');
+                        }
+                    }
+                }
+            })
+        });
+
+    </script>
 @endsection

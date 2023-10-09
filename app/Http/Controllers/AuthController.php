b3e20db8e317bf305fa8230854dca08ab70cf7b5
toan.dg <toan.dg>
2023-10-09 16:54:13 +0700

    add 54

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ResetPasswordEmail;
 use App\Models\Country;
 use App\Models\CustomerAddress;
 use App\Models\Order;
@@ -11,7 +12,9 @@ use App\Models\Wishlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -247,7 +250,6 @@ class AuthController extends Controller
     }
 
     public function showChangePasswordForm() {
-
         return view('front.account.change-password');
     }
 
@@ -291,5 +293,90 @@ class AuthController extends Controller
         }
 
         return view('front.account.change-password');
+    }
+
+    public function forgotPassword() {
+        return view('front.account.forgot-password');
+    }
+
+    public function processForgotPassword(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users,email',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('front.forgotPassword')->withInput()->withErrors($validator);
+            // ->withInput(): Hàm này sẽ giữ lại dữ liệu mà người dùng đã nhập.
+            // ->withErrors($validator): Hàm này sẽ chuyển các thông báo lỗi từ validator vào session để có thể truy cập từ view.
+        }
+
+        $token = Str::random(60);
+        // Lớp Str chứa nhiều phương thức hữu ích để thao tác với chuỗi.
+
+        \DB::table('password_reset_tokens')->where('email', $request->email)->delete();
+
+        \DB::table('password_reset_tokens')->insert([
+            'email' => $request->email,
+            'token' => $token,
+            'created_at' => now(),
+        ]);
+
+        // Send Email Here
+
+        $user = User::where('email', $request->email)->first();
+
+        $formData = [
+            'token' => $token,
+            'user' => $user,
+            'mailSubject' => 'You have requested to reset your password.',
+        ];
+
+        Mail::to($request->email)->send(new ResetPasswordEmail($formData));
+
+        return redirect()->route('front.forgotPassword')->with('success', 'Please check your inbox to reset password.');
+    }
+
+    public function resetPassword($token) {
+
+        $tokenExist = \DB::table('password_reset_tokens')->where('token', $token)->first();
+
+        if ($tokenExist == null) {
+            return redirect()->route('front.forgotPassword')->with('error', 'Invalid requested');
+        }
+
+        return view('front.account.reset-password', [
+            'token' => $token,
+        ]);
+    }
+
+    public function processResetPassword(Request $request) {
+        $token = $request->token;
+
+        $tokenObj = \DB::table('password_reset_tokens')->where('token', $token)->first();
+
+        if ($tokenObj == null) {
+            return redirect()->route('front.forgotPassword')->with('error', 'Invalid requested');
+        }
+
+        $user = User::where('email', $tokenObj->email)->first();
+
+        $validator = Validator::make($request->all(), [
+            'new_password' => 'required|min:5',
+            'confirm_password' => 'required|same:new_password',
+        ]);
+
+        if ($validator->fails()) {
+
+            return redirect()->route('front.resetPassword', $token)->withErrors($validator);
+        }
+
+        User::where('id', $user->id)->update([
+            'password' => Hash::make($request->new_password),
+        ]);
+
+        \DB::table('password_reset_tokens')->where('email', $user->email)->delete();
+
+        return redirect()->route('account.login')->with('success', 'You have successfully updated your password.');
     }
 }
